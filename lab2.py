82d5f168301d631c869588415e98038f348738c3
Daniel Solus <dsolus@github.com>
2016-09-13 07:06:26 -0500

    cleaned up, ready to submit

--- lab2.py
+++ lab2.py
@@ -1,7 +1,10 @@
 #!usr/bin/env python 3.5
 #Daniel Solus
 #Assignment LAB 2
-"""You have three tries to hit the sheep with a hay bale. Choose an angle and velocity."""
+"""You have three trys to hit the sheep with a hay bale. Choose an angle and velocity. 
+This program calls the function sheep_laucher three times. The function allows you to choose
+an intial velocity and angle for the launcher. If the hay falls within 50 meters of the sheep
+you win the game"""
 import math
 
 #The sheep is 300 meters away
@@ -10,29 +13,28 @@ def sheep_launcher(*args):
     sheep = 300
 
 #Prompt the user for velocity and the angel of the launcher.
-    v = input('Set the velocity of the launcher')
-    theta = input('Set the angle of the launcher')
+    v = input('Set the velocity of the launcher (m/s) ')
+    theta = input('Set the angle of the launcher (degrees) ')
 
 #convert theta to radians
     radians = math.radians(float(theta))
 
-    print(v, radians)
-
 #Input the data into the equation to find delta x.
     v = float(v)
     deltax = (2*v**2*math.sin(radians)*math.cos(radians))/g
-    print(deltax)
+    print('deltax: {} meters'.format(deltax))
 
 #Check to see if the hay bale lands within 50 meters of the sheep.
 #print the distance missed by the user if the sheep is not hit
     if deltax == 300:
         print('You hit the sheep!')
     elif deltax < 249:
-        print('You missed by {}'.format(249 - deltax))
+        print('You missed! The hay was short by {} meters.'.format(249 - deltax))
     elif deltax > 350:
-        print('You missed by {}'.format(deltax - 350))
+        print('You missed! The hay was long by {} meters.'.format(deltax - 350))
     else:
-        print('You feed the sheep!')
+        print('You fed the sheep!')
+    return
 
 #Calling the sheep_laucher fucntion for 3 trys.
 print("You have three trys to hit the sheep. Good Luck!")
