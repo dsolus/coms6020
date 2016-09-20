0b0a9e5c4027135177f92fc9438e392443e53cf4
Daniel Solus <dsolus@github.com>
2016-09-20 06:42:48 -0500

    completed assignment, tiles size still incorrect

--- lab3.py
+++ lab3.py
@@ -3,7 +3,7 @@
 #Assignment LAB 3
 """Write a program that computes the number of tiles, the number of boxes of tiles, and the
 number of extra tiles for a room."""
-
+import math
 
 def tile_conv(*args):
     #program should prompt the user for the number of tiles in inches.
@@ -11,36 +11,47 @@ def tile_conv(*args):
     if tile_size <= 24 and tile_size >= 4:
         print (tile_size)
     else:
-        print("Invalid tile size.\nTile size should be between 4 and 24 inches.\n Good bye.")
+        print("Invalid tile size.\nTile size should be between 4 and 24 inches.\nGood bye.")
+        quit()
 
     #Enter the width of the room
     room_width_ft = input("Enter room width in feet: ")
     room_width_in = input("and inches: ")
+    if room_width_in >= 12:
+        print("Number of inches must be less than 12.\nGood bye.")
+        quit()
     #Convert room width to inches
-    room_width_ft = room_width_ft * 12
+    room_width_ft = room_width_ft * 12.0
     room_width = room_width_ft + room_width_in
-
     #Enter the length of the room
     room_length_ft = input("Enter the room length in feet: ")
     room_length_in = input("and inches: ")
+    if room_length_in >= 12:
+        print("Number of inches must be less than 12.\nGood bye.")
+        quit()
     #Convert room length to inches
-    room_length_ft = room_length_ft * 12
+    room_length_ft = room_length_ft * 12.0
     room_length = room_length_ft + room_length_in
 
-    #convert room size to inches
+    #calculate number of tiles for the room
     tile_width = room_width / tile_size
+    tile_width = math.ceil(tile_width)
     print(tile_width)
-    if room_width % tile_size != 0:
-        tile_width  = tile_width + 1
 
     tile_length = room_length / tile_size
+    tile_length = math.ceil(tile_length)
     print(tile_length)
-    if room_length % tile_size != 0:
-        tile_width  = tile_width + 1
 
     total_tiles = tile_width*tile_length
 
-    # a box contains 24 tiles
+    #copmute the number of boxes of tile, a box contains 24 tiles.
+    boxes = math.ceil(total_tiles / 24)
+    print("The number of boxes of tile needed to cover the room: {}".format(boxes))
+
+    #the number of extra tiles for a room
+    extra = (24 * boxes) - int(total_tiles)
+    print("The extra tiles leftover: {}".format(extra))
+
     print("The total number of tiles needed to cover the room: {}".format(total_tiles))
 
 tile_conv()
